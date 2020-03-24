deb6cc2e37248eaa5fe93667b8f5ff2f5806ecae
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-03-24 17:06:32 -0600

    added npm

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function authenticator(req, res, next) {
 
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
-  if (username === 'Lambda School' && password === 'i<3Lambd4') {
+  if (username === 'Alexander' && password === 'joemama') {
     req.loggedIn = true;
     res.status(200).json({
       payload: token
